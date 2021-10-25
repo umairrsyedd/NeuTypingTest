f33c85b62b7c617856404f695be696ec56a3f476
Syed Umair <umair_developer@outlook.com>
2021-10-25 17:09:40 +0530

    Added Random Easy Word Generator Utility Function

---
+++ src/Utils/GenerateText.js
@@ -0,0 +1,9 @@
+
+export default function GenerateText(Text) {
+    const randomWords = [];
+    for (let i = 0; i < 30; i++) {
+        const randomIndex = Math.floor(Math.random() * Text.length);
+        randomWords.push(Text[randomIndex]);
+    }
+    return randomWords.join(' ');
+}
